0633e7ba7839b5e3de331e4374b3c2244139597b
BistonN <biston.nunes@gmail.com>
2023-09-26 19:31:14 -0300

    subindo dados dos produtos

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ navegador.get("https://lista.mercadolivre.com.br/raquete-beach-tennis")
 time.sleep(10)
 
 urls = []
-for i in range(2):
+for i in range(1):
     produtos_pagina = navegador.find_elements(By.CSS_SELECTOR, '.andes-card')
     for produto in produtos_pagina:
         url = produto.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
@@ -16,10 +16,13 @@ for i in range(2):
     id = 3 if i == 0 else 4
     proximo = navegador.find_element(By.XPATH, f'//*[@id="root-app"]/div/div[2]/section/div[9]/nav/li[{id}]/a').get_attribute("href")
     navegador.get(proximo)
-    time.sleep(10)
+    time.sleep(5)
 
 produtos = []
+a = 0
 for url in urls:
+    a += 1
+    print(a)
     navegador.get(url)
     produtos.append({
         'nome': navegador.find_element(By.CSS_SELECTOR, 'h1.ui-pdp-title').text,
@@ -29,6 +32,7 @@ for url in urls:
         'marca': navegador.find_element(By.CSS_SELECTOR,'.andes-table__body').text,
         'url': url
     })
-    time.sleep(5)
+    # time.sleep(4)
 
+pd.DataFrame(produtos).to_excel('produtos.xlsx')
 print(urls)
